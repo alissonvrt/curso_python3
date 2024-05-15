cd3b892e641d2c3da7110deed0454752a675d36e
josé _Alisson_dev <121328136+alissonvrt@users.noreply.github.com>
2024-05-15 10:40:41 -0300

    exercicio_deepcopy

---
+++ aula_exercicio100.py
@@ -0,0 +1,36 @@
+# copy, sorted, produtos.sort
+# Exercícios
+# Aumente os preços dos produtos a seguir em 10%
+# Gere novos_produtos por deep copy (cópia profunda)
+produtos = [
+    {'nome': 'Produto 5', 'preco': 10.00},
+    {'nome': 'Produto 1', 'preco': 22.32},
+    {'nome': 'Produto 3', 'preco': 10.11},
+    {'nome': 'Produto 2', 'preco': 105.87},
+    {'nome': 'Produto 4', 'preco': 69.90},
+]
+
+# Ordene os produtos por nome decrescente (do maior para menor)
+# Gere produtos_ordenados_por_nome por deep copy (cópia profunda)
+
+# Ordene os produtos por preco crescente (do menor para maior)
+# Gere produtos_ordenados_por_preco por deep copy (cópia profunda)
+
+import copy
+
+aumenta_preco = sorted(    # Aumenta os preços dos produtos a seguir em 10%
+    copy.deepcopy(produtos),
+    key=lambda p: p['nome'],  # aumentou o preco em 10%
+    reverse=True
+)
+
+
+produtos_ordenados_por_preco = sorted(  #ordena os produtos por preço
+    produtos,
+    key=lambda x: x['preco']
+)
+
+
+print(aumenta_preco)
+print( )
+print(produtos_ordenados_por_preco)
